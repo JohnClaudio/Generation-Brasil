95d382524fe0d333174add6104d43f02e3060b5d
Johnclaudio <kidizinhu200@gmail.com>
2021-09-03 23:37:19 -0300

    03-09-2021

---
+++ Algoritmos/Arrays/Matriz3x3.java
@@ -0,0 +1,46 @@
+package GenerationTurma32.Algoritmos.Arrays;
+/*
+3- Leia uma matriz 3 x 3, conte e escreva quantos valores maiores que 10 ela possui.
+*/
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Matriz3x3 {
+
+
+
+    public static void main(String[] args) {
+        int numero[][] = new int [3][3];
+        Scanner leia = new Scanner(System.in);
+        int qtdValoresMaioresqueDez=0;
+
+        try
+        {
+
+            for (int i=0;i<=2;i++)
+            {
+                    for (int b=0; b<=2; b++)
+                    {
+                        System.out.println("Preencha o valor da linha: " + i + " e coluna: " + b );
+                        numero[i][b] = leia.nextInt();
+
+                        if(numero[i][b]>10)
+                        {
+                            qtdValoresMaioresqueDez++;
+                        }
+
+                    }
+            }
+            System.out.println("A matriz possui: " + qtdValoresMaioresqueDez + " valores superiores a 10");
+
+        }
+        catch (InputMismatchException error)
+        {
+
+            System.out.println("Você precisa digitar um numero inteiro válido");
+        }
+
+
+    }
+}
