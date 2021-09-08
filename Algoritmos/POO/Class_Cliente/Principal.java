e7756588bee29dcab29a42be28962ed193d10679
Johnclaudio <kidizinhu200@gmail.com>
2021-09-08 16:33:42 -0300

    POO

---
+++ Algoritmos/POO/Class_Cliente/Principal.java
@@ -0,0 +1,14 @@
+package GenerationTurma32.Algoritmos.POO.Class_Cliente;
+
+/*
+1) Crie uma classe cliente e apresente os atributos e métodos referentes
+esta classe, em seguida crie um objeto cliente, defina as instancias deste
+objeto e apresente as informações deste objeto no console.*/
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Cliente cliente = new Cliente("Marcela", "48022060892", "5671-92530", "email@email.com");
+        cliente.ImprimirDados();
+    }
+}
---
+++ Algoritmos/POO/ProdutoEletronico/Principal.java
@@ -0,0 +1,15 @@
+package GenerationTurma32.Algoritmos.POO.ProdutoEletronico;
+
+/*
+3) Crie uma classe produto eletrônico e apresente os atributos e métodos
+referentes esta classe, em seguida crie um objeto produto eletrônico,
+defina as instancias deste objeto e apresente as informações deste objeto
+no console.*/
+
+public class Principal {
+
+    public static void main(String[] args) {
+      ProdutoEletronico micro_ondas = new ProdutoEletronico("Microondas Eletrolux",300.50,"25/10/2020","Eletrodomestico");
+      micro_ondas.imprimirDados();
+    }
+}
